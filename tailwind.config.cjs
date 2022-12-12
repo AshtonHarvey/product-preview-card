ae8c00839546fdb6f32d1d366e7c6fb67e76bdb3
Ashton Harvey <Ashtonh36@gmail.com>
2022-12-12 18:36:14 -0500

    create mobile design. create tailwind config themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      "Dark-cyan": "hsl(158, 36%, 37%)",
+      Cream: "hsl(30, 38%, 92%)",
+      "Very-dark-blue": "hsl(212, 21%, 14%)",
+      "Dark-grayish-blue": "hsl(228, 12%, 48%)",
+      White: "hsl(0, 0%, 100%)",
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".1em",
+      "widest-two": ".25em",
+      "widest-three": ".4em",
+    },
+    extend: {
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
